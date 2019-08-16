a64362c97e6fb169da359433a6038e464b23374d
Elvis Knapman <elvis@elvisknapman.com>
2019-08-16 16:32:08 -0400

    wrote action creator for marking todos as completed

--- todo-with-redux/src/actions/index.js
+++ todo-with-redux/src/actions/index.js
@@ -1,10 +1,18 @@
 // action types
 export const ADD_TODO = "ADD_TODO";
+export const MARK_TODO_COMPLETED = "MARK_TODO_COMPLETED";
 
 // action creators
 export const addNewTodo = todo => {
   return {
     type: ADD_TODO,
+    payload: todo
+  };
+};
+
+export const markTodoCompleted = todo => {
+  return {
+    type: MARK_TODO_COMPLETED,
     payload: todo
   };
 };
