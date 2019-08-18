280460476c3ccfaf58f64b71798b4a3c7f137a6a
Elvis Knapman <elvis@elvisknapman.com>
2019-08-17 20:33:53 -0400

    Added Material ui imports. Created style object, applied to..
    material ui component props

--- todo-with-redux/src/components/AddTodo.js
+++ todo-with-redux/src/components/AddTodo.js
@@ -4,6 +4,21 @@ import { connect } from "react-redux";
 // actions
 import { addNewTodo } from "../actions";
 
+// Material UI
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const styles = {
+  input: {
+    fontSize: "2rem"
+  },
+
+  label: {
+    fontSize: "2rem"
+  }
+};
+
 const AddTodo = props => {
   const [todoText, setTodoText] = useState("");
 
@@ -15,16 +30,30 @@ const AddTodo = props => {
 
   return (
     <div>
-      <h1>Add New Todo</h1>
+      <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="todo"
-          onChange={e => setTodoText(e.target.value)}
-          value={todoText}
-          placeholder="New todo..."
-        />
-        <button type="submit">Add Todo</button>
+        <div className="todo-text-field">
+          <TextField
+            fullWidth
+            label="New Todo"
+            InputProps={{ style: styles.input }}
+            InputLabelProps={{ style: styles.label }}
+            type="text"
+            name="todo"
+            onChange={e => setTodoText(e.target.value)}
+            value={todoText}
+          />
+        </div>
+        <div>
+          <Button
+            style={{ fontSize: "1.3rem" }}
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Add New Todo
+          </Button>
+        </div>
       </form>
     </div>
   );
