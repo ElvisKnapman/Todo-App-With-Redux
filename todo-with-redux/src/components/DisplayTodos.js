edec6bf5c36cc58ab98eaaebc1419d6422143cd5
Elvis Knapman <elvis@elvisknapman.com>
2019-08-18 18:56:47 -0400

    Imported Paper and Typography from Material UI...
    styled components.. used Paper component for header and Typography component for text

--- todo-with-redux/src/components/DisplayTodos.js
+++ todo-with-redux/src/components/DisplayTodos.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
 
+// Material UI
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+// Material UI useStyles
+// import useStyles from "../material/styles";
+
 // components
 import TodoItem from "./TodoItem";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  h3: {
+    fontSize: "2.7rem",
+    padding: "1rem 2rem",
+    backgroundColor: "#39C",
+    color: "#fff"
+  }
+}));
 
 const DisplayTodos = ({ todos }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1 className="todo-category-header">Todo</h1>
+    <>
+      <Paper classes={{ root: classes.todoHeaders }}>
+        <Typography variant="h3" align="center" classes={{ h3: classes.h3 }}>
+          Todo
+        </Typography>
+      </Paper>
       {todos.length > 0
         ? todos.map(todo => {
             return <TodoItem key={todo.id} todo={todo} />;
           })
         : "You currently have no items in your todo list."}
-    </div>
+    </>
   );
 };
 
