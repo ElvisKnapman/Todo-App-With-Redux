b6c04eb50e29ebab250a5272a0708e1b0035b072
Elvis Knapman <elvis@elvisknapman.com>
2019-08-18 20:20:10 -0400

    Built out item to display in a card style

--- todo-with-redux/src/components/CompletedTodos/CompletedTodoItem.js
+++ todo-with-redux/src/components/CompletedTodos/CompletedTodoItem.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Moment from "react-moment";
+
+// Material UI
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+// Custom styles
+import useStyles from "../../component-styles/todoItems";
 
 const CompletedToDoItem = ({ todo }) => {
-  return <p>{todo.title}</p>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <Card className={classes.card}>
+        <CardContent classes={{ root: classes.cardContent }}>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            {todo.title}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Created: <Moment fromNow>{todo.createdAt}</Moment>
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default CompletedToDoItem;
