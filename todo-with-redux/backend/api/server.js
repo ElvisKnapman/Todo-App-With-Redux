0e526e8c6b57447116646ec9797a9d6a8bd5f107
Elvis Knapman <elvis@elvisknapman.com>
2019-10-11 15:01:06 -0400

    :sparkles: added routes for users and todos

--- todo-with-redux/backend/api/server.js
+++ todo-with-redux/backend/api/server.js
@@ -3,7 +3,13 @@ const express = require("express");
 
 const server = express();
 
+// import routes
+const usersRoutes = require("../routes/usersRoutes");
+const todosRoutes = require("../routes/todosRoutes");
+
 server.use(express.json());
+server.use("/api/users", usersRoutes);
+server.use("/api/todos", todosRoutes);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API is up..." });
