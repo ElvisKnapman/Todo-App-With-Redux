87d5b027c731d91b39493167fa13f6ca01656e88
Elvis Knapman <elvis@elvisknapman.com>
2019-10-11 15:02:53 -0400

    :sparkles: wrote helper functions for todos api
    helpers for creating a todo, finding a todo by its id and finding all todos for a specific user

---
+++ todo-with-redux/backend/helpers/todoHelpers.js
@@ -0,0 +1,28 @@
+const db = require("../data/db-config");
+
+function createTodo(todo) {
+  return db("todos")
+    .insert(todo, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("todos")
+    .where({ id })
+    .first();
+}
+
+function findByUser(id) {
+  return db("todos")
+    .join("users", "users.id", "todos.user_id")
+    .where({ "users.id": id })
+    .select("todos.id", "todos.todo", "todos.created_at", "todos.updated_at");
+}
+
+module.exports = {
+  createTodo,
+  findById,
+  findByUser
+};
