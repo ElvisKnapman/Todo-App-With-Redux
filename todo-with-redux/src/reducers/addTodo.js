c056719e0212bc27359bccd7133f8e75fd5fc92c
Elvis Knapman <elvis@elvisknapman.com>
2019-10-11 21:17:15 -0400

    :construction: rewriting addTodo reducer

---
+++ todo-with-redux/src/reducers/addTodo.js
@@ -0,0 +1,51 @@
+// action types
+import {
+  ADD_TODO_START,
+  ADD_TODO_SUCCESS,
+  ADD_TODO_FAILURE
+} from "../actionCreators/addTodo";
+
+// moment for timestamp
+import moment from "moment";
+
+const initialState = {
+  isAdding: false
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO_START:
+      return {
+        ...state,
+        isAdding: true
+      };
+
+    case ADD_TODO_SUCCESS:
+      return {
+        ...state,
+        isAdding: false
+      };
+
+    case ADD_TODO_FAILURE:
+      return {
+        ...state,
+        isAdding: false
+      };
+
+    // case MARK_TODO_COMPLETED:
+    //   return {
+    //     ...state,
+    //     todos: state.todos.map(todo => {
+    //       if (todo.id === action.payload.id) {
+    //         return { ...todo, completed: true, completedAt: moment() };
+    //       }
+    //       return todo;
+    //     })
+    //   };
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
