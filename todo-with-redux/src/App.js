1fe9155bee430f782cbd57c5eaadb47b1bef7490
Elvis Knapman <elvis@elvisknapman.com>
2019-10-13 15:28:53 -0400

    :art: extracted UserHomepage out to its own component

--- todo-with-redux/src/App.js
+++ todo-with-redux/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 // CSS
 import "./sass/index.scss";
@@ -8,58 +8,22 @@ import "./sass/index.scss";
 import Moment from "react-moment";
 
 // Components
-import AddTodo from "./components/AddTodo";
-import DisplayTodos from "./components/DisplayTodos";
-import DisplayCompletedTodos from "./components/CompletedTodos/DisplayCompletedTodos";
-
-// Material UI
-import {
-  createMuiTheme,
-  MuiThemeProvider,
-  makeStyles
-} from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#39C"
-    },
-    secondary: {
-      main: "#000"
-    }
-  },
-  spacing: 10
-});
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    marginTop: "5rem"
-  }
-}));
+import RegisterForm from "./components/Register/RegisterForm";
+import LoginForm from "./components/Login/LoginForm";
+import UserHomepage from "./components/UserHomepage";
 
 Moment.startPooledTimer(30000);
 
 function App() {
-  const classes = useStyles();
   return (
-    <MuiThemeProvider theme={theme}>
-      <div className="app-container">
-        <div className="add-todo-container">
-          <AddTodo />
-        </div>
-        <Container classes={{ root: classes.container }} maxWidth="md">
-          <Grid container spacing={4}>
-            <Grid item xs={6}>
-              <DisplayTodos />
-            </Grid>
-            <Grid item xs={6}>
-              <DisplayCompletedTodos />
-            </Grid>
-          </Grid>
-        </Container>
-      </div>
-    </MuiThemeProvider>
+    <Router>
+      <Switch>
+        <Route path="/register" component={RegisterForm} />
+        <Route path="/login" component={LoginForm} />
+
+        <Route path="/home" component={UserHomepage} />
+      </Switch>
+    </Router>
   );
 }
 
